669401bd37a04a150f4b1737532b247dda6496c4
Bucha1958 <okoriemmadu@gmail.com>
2022-09-23 13:55:10 +0000

    Add slicing on your code

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word[:3]
+word[3:4]
+word[-1:]
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[2:6]}")
