cc738a22acb20e9378909b417ce1c2c24f3bf2c7
Bucha1958 <okoriemmadu@gmail.com>
2022-12-07 05:30:58 +0000

    Delete

--- 0x04-python-more_data_structures/2-main.py
+++ 0x04-python-more_data_structures/2-main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = {'language': "C", 'number': 13, 'track': "Low level"}
-nb_keys = print_sorted_dictionary(a_dictionary)
-print("Number of keys: {}".format(nb_keys))
+a_dictionary = {'language': "C", 'number': 13, 'track': "Low level",'ids': [1, 2, 3]}
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+
+new_dict = simple_delete(a_dictionary, 'number')
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
 
 
 
