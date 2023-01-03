a525ae461d7c7c1071a0a58532b1cd4377b7f716
Bucha1958 <okoriemmadu@gmail.com>
2023-01-03 10:15:59 +0000

    Hello

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    You are not allowed to import any module
+"""
+
+
+class Square():
+    """
+        class that defines a square
+    """
+    def __init__(self, size):
+        """
+            private instance attribute
+        """
+        self.__size = size
