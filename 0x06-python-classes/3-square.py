8fcd7a48d14a86ad651cae8105df88152118fc06
Bucha1958 <okoriemmadu@gmail.com>
2023-01-04 19:23:23 +0000

    Private instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Importing of module is not allowed
+"""
+
+
+class Square():
+    """
+        Class that defines a square
+    """
+    def __init__(self, size=0):
+        """
+            Instantiation
+        """
+        self.__size = size
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """
+            Public instance method
+        """
+        return self.__size ** 2
