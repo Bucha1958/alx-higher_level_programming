a7b5e20ede8537d287a6acc0ef2fb346476ebc12
Bucha1958 <okoriemmadu@gmail.com>
2023-02-04 13:33:06 +0000

    lazy matrix

---
+++ 0x07-python-test_driven_development/101-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
+
+matrix1 = [[12, 3],
+        [4, 6]]
+
+matrix2 = [[5, 1],
+        [6, 7]]
+
+print(lazy_matrix_mul(matrix1, matrix2))
+
