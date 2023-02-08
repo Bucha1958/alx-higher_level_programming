b4bd13c8bf179dbf2b6feb1b03ee294ab9b8e6d8
Bucha1958 <okoriemmadu@gmail.com>
2023-02-08 17:06:25 +0100

    Rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+No importation of module
+"""
+
+
+class Rectangle():
+    """
+    Class rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialization
+        """
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Getter
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+    @property
+    def height(self):
+        """
+        Getter
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise TypeError("height must >= 0")
+
+        self.__height = value
+        
+
+    
