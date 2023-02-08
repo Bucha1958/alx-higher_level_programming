725cb7618976c4a7ba80ca57f7362a27c27fa4a5
Bucha1958 <okoriemmadu@gmail.com>
2023-02-08 16:13:34 +0100

    Empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+No module imported
+"""
+
+
+
+class Rectangle:
+    """
+    class rectangle
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+## Object Oriented Programming
+
+More on objects and classes
