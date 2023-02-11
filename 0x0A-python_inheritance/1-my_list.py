7ecc5ba31b9a28235574a6f5014de8a08a87499b
Bucha1958 <okoriemmadu@gmail.com>
2023-02-11 17:03:42 +0100

    Inheritance

---
+++ 0x0A-python_inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+No importation of module
+"""
+
+
+class MyList():
+    """
+    Class MyList inherits from the class list
+    """
+
+    def __init__(self):
+        pass
+    def print_sorted(self):
+        print(sorted(self))
