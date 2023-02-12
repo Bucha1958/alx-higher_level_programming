09ff106789dff553662b43b8e94b7dd3a262a5b1
Bucha1958 <okoriemmadu@gmail.com>
2023-02-12 14:13:31 +0100

    Raise Exception

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+No importation of module
+"""
+
+class BaseGeometry():
+    """
+    Empty class BaseGeometry
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+No importation of module
+"""
+
+
+class BaseGeometry():
+    """
+    class BaseGeometry created
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an eception
+        """
+
+        raise Exception("area() is not implemented")
