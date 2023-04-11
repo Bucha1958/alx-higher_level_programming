daa200b690c5a607bae698723cf57ef683c620e9
Bucha1958 <okoriemmadu@gmail.com>
2023-04-11 19:59:28 -0400

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const process = require('process');
+let args = process.argv;
+if (args.length < 3)
+{
+	console.log("No argument");
+}
+else if (args.length == 3)
+{
+	console.log("Argument found");
+}
+else
+{
+	console.log("Arguments found");
+}
