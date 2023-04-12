f972c433500f84f247bb273d797241e7527bbdb3
Bucha1958 <okoriemmadu@gmail.com>
2023-04-11 21:48:38 -0400

    Loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let container = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let count = 0;
+while (count < container.length) {
+	console.log(container[count]);
+	count++;
+}
