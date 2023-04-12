07a977567ce59d0263a8dccec2b6dfb8f16bdcdc
Bucha1958 <okoriemmadu@gmail.com>
2023-04-11 21:19:56 -0400

    To integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require("process");
+let args = process.argv;
+let myVal = parseInt(args[2]);
+if (args.length < 3) {
+	console.log("Not a number");
+} else {
+	if (myVal)
+	{
+		console.log("My number:" + myVal);
+	} else {
+		console.log("Not a number");
+	}
+}
