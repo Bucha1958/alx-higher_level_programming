bbabbefcb5424dadef7741ac94ae36424a586f88
Bucha1958 <okoriemmadu@gmail.com>
2023-04-16 18:01:30 +0000

    Filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,16 +2,20 @@
 import MySQLdb
 from sys import argv
 
-db = MySQL.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name = '{:s}'".format(argv[4]))
-results = cursor.fetchall()
-for result in results:
-    print(result)
-db.close()
+if (len(argv) < 4 or len(argv) > 5):
+    raise IndexError("Arguments must be five")
+else:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{:s}'".format(argv[4]))
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+    db.close()
 
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if (len(argv) < 5 or len(argv) > 5):
+    pass
+try:
+    conn = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+        )
+except:
+    exit(1)
+cur = conn.cursor()
+cur.execute("SELECT * FROM `states`\
+            WHERE `name`=(%s) ORDER BY `id` ASC", (argv[4],))
+results = cur.fetchall()
+for result in results:
+    print(result)
+cur.close()
+conn.close()
