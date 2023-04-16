a59cc411aafc5aea52c177ac192b55308374df53
Bucha1958 <okoriemmadu@gmail.com>
2023-04-16 18:59:04 +0000

    Primary key

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if (len(argv) == 4):
+    try:
+        connector = MySQLdb.connect(
+                                    host="localhost",
+                                    port=3306,
+                                    user=argv[1],
+                                    passwd=argv[2],
+                                    db=argv[3],
+                                    charset="utf8"
+                                    )
+    except IndexError:
+        pass
+    cur = connector.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities INNER JOIN states ON cities.state_id = states.id")
+    results = cur.fetchall()
+    for result in results:
+        print(result)
+    connector.close()
+
