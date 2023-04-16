98070fb65491908f69f1ff3dfd5e60ec2af7c682
Bucha1958 <okoriemmadu@gmail.com>
2023-04-16 11:55:24 +0000

    Add to git

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+def print_states(username, password, db_name):
+    db = MySQLdb.connect(host="localhost",
+                        user=username,
+                        passwd=password,
+                        db=db_name,
+                        port=3306,
+                        charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    outputs = cursor.fetchall()
+    for output in outputs:
+        print(output)
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    credentials = argv
+    username = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    print_states(username, passwd, db_name)
+
