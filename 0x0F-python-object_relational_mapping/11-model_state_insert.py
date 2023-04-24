3a1ece0a94ef52f2c12dd565bdf25d2e35385753
Bucha1958 <okoriemmadu@gmail.com>
2023-04-24 09:53:52 +0000

    Added object

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -6,4 +6,15 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), post_pre_ping = True)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmake(bind=engine)
+    session = Session()
+    state = State()
+    state.id = 6
+    state.name = "Louisiana"
+    session.add(state)
+    session.commit()
+    results = session.query(State).order_by(State.id).all()
+    for result in results:
+        print("{}".format(result.id))
