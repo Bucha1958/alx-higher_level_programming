3c2c40452d354f613e045b08a3c8ddda17c536d2
Bucha1958 <okoriemmadu@gmail.com>
2023-04-24 06:41:19 +0000

    First

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping = True)
     Base.metadata.create_all(bind=engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
@@ -16,3 +15,4 @@ if __name__ == "__main__":
         print("{}: {}".format(item.id, item.name))
     except:
         print()
+
