e4e9bdf9cfc21b30eccb62311626350998abf9f8
Bucha1958 <okoriemmadu@gmail.com>
2023-04-30 10:27:50 -0700

    Task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # sends a request to a URL, and displays the size of the body of the response
-response=$(curl -sS -w '%{size_download}' -o text.txt $1)
-echo "$response"
+curl -sS -w '%{size_download}' -o text.txt $1
+
 
