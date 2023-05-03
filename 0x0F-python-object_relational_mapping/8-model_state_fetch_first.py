5becabff76c1ebb3205ceaa6cf88aa77c586b2fe
Bucha1958 <okoriemmadu@gmail.com>
2023-05-03 19:48:33 +0000

    New script

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,13 +6,13 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping = True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        item = session.query(State).first()
-        print("{}: {}".format(item.id, item.name))
-    except:
-        print()
-
+    item = session.query(State).first()
+    print("{}: {}".format(item.id, item.name))
