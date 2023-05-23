94031456340cab2ae04ea23e3c348f1c5e071002
Bucha1958 <okoriemmadu@gmail.com>
2023-05-23 21:12:21 +0000

    corrected file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let filePath = process.argv[2];
+const filePath = process.argv[2];
 
-let fs = require('fs');
+const fs = require('fs');
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data.toString());
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
