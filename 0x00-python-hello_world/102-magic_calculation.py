459e4727d6737941d1a505468c10ac5f65af6651
Bucha1958 <okoriemmadu@gmail.com>
2023-06-15 09:40:19 +0100

    Bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """calculation"""
 
-def magic_calculation(a, b=98):
-    """does exactly same as bytecode given"""
-    return a + b
-    return a ** b
+def magic_calculation(a, b):
+    result = 98
+    result += a ** b
+    return result
+
