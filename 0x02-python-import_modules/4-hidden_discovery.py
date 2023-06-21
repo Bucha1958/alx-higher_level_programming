e1ba912cfb581aae02b95e395e127e3b45f5f74d
Bucha1958 <okoriemmadu@gmail.com>
2023-06-21 12:09:39 -0700

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+
 import hidden_4
 
-
-def print_defined_names():
-    names = []
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
-            names.append(name)
-
-    for name in sorted(names):
-        print(name)
-
-
-print_defined_names()
+if __name__ == "__main__":
+    for d in dir(hidden_4):
+        if "__" not in d:
+            print(d)
